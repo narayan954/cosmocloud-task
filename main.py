0e406f56bba4edab21246939ffa83b252ae1ea80
Narayan soni <narayansoni854@gmail.com>
2023-04-23 13:57:43 +0530

    adds db client

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 app = FastAPI()
+client = MongoClient("mongodb://localhost:27017/")
+db = client["mydatabase"]
+users = db["users"]
 
 
 @app.get("/")
